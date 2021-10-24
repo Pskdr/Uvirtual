c52b309df09679582e201c5c1cea236814e33628
Pskdr <juanca.naranjo03@gmail.com>
2021-10-24 11:53:17 -0500

    Login terminado y arreglado

--- src/App.js
+++ src/App.js
@@ -3,33 +3,23 @@ import './App.css';
 import React,{useState} from 'react';
 import Navbar from './components/Navbar';
 import area_personal from './components/area-personal';
-import styled from '@emotion/styled';
 import Formulario from './components/Formulario';
 import { Fragment } from 'react/cjs/react.production.min';
 
-const DivImage = styled.div`
-  background-position: center center;
-  background-color: #f1faee;
-  width: 600px;
-  text-align: center;
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 10rem;
-`;
 
 function App() {
-  const [login, actualizarLogin] = useState(true)
+  const [ login, actualizarLogin ] = useState(true)
   return (
     <div className="divImagen">
     <Navbar/>
         
         {login ?(
-                  <DivImage>
-                    <Formulario
+            <div className="divImagen">
+              <Formulario
                     actualizarLogin={actualizarLogin}
                     />
-                  </DivImage>
+            </div>
+                    
                   
           
          ):
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import { Fragment } from 'react';
 import './Formulario.css'
-import logo from '../images/iniciarSesionOutlook.jpeg'
 import Form from './Form'
 
 const Formulario = ({actualizarLogin}) => {
     return ( 
         <Fragment>
-            
-            <div class="card text-center">
-                <div class="card-header">
-                    <h1>Accede a la plataforma</h1>
-                </div>
-                <div class="card-body">
-                    
-                    <Form/>
-                </div>
-                <div class="card-footer text-muted">
-                <button type="button" class="btn btn-secondary">Acceder como invitado</button>
-                </div>
+            <div className="formulario">
+                <h2>Acceso a la plataforma</h2>                
+                <Form/>    
             </div>
         </Fragment>
         
